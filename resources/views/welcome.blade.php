51cb04a78aa4a1944a4bc5b43acdfec156fc4878
B Ambrose <benjamin.ambrose2003@yahoo.com>
2023-02-14 16:19:06 +0000

    all text positioning good

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Weather App</title>
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="/resources/css/app.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"></script>
-    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
+    </script>
     <script>
         tailwind.config = {
             theme: {
@@ -27,11 +26,6 @@
             }
         }
     </script>
-    <style>
-        .col-2 {
-            align-items: center;
-        }
-    </style>
     <style type="text/tailwindcss">
         @layer utilities {
             .content-auto {
@@ -67,7 +61,7 @@
                     <div class="col-7 flex-items-center">
                         <div>Clear Sky</div>
                     </div>
-                    <div class="col-2 text-right text-xs">
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
                         <div>4&#176;C</div>
                         <div>4&#176;C</div>
                     </div>
@@ -81,7 +75,7 @@
                     <div class="col-7 flex-items-center">
                         <div>Clear Sky</div>
                     </div>
-                    <div class="col-2 text-right text-xs">
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
                         <div>4&#176;C</div>
                         <div>4&#176;C</div>
                     </div>
@@ -95,7 +89,7 @@
                     <div class="col-7 flex-items-center">
                         <div>Clear Sky</div>
                     </div>
-                    <div class="col-2 text-right text-xs">
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
                         <div>4&#176;C</div>
                         <div>4&#176;C</div>
                     </div>
@@ -109,7 +103,7 @@
                     <div class="col-7 flex-items-center">
                         <div>Clear Sky</div>
                     </div>
-                    <div class="col-2 text-right text-xs">
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
                         <div>4&#176;C</div>
                         <div>4&#176;C</div>
                     </div>
@@ -123,7 +117,7 @@
                     <div class="col-7 flex-items-center">
                         <div>Clear Sky</div>
                     </div>
-                    <div class="col-2 text-right text-xs">
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
                         <div>4&#176;C</div>
                         <div>4&#176;C</div>
                     </div>
