f40d33a798a75a1fcfa0dc8637b9c5a924ee5c85
vagrant <vagrant@homestead>
2023-02-14 15:11:32 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    theme: {
+        extend: {
+            spacing: {
+                '128': '32rem',
+            },
+            gridTemplateColumns: {
+                'weather': '3fr 6fr 3fr',
+            }
+        },
+    },
+    variant: {},
+    plugins: [],
+}
