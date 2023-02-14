f52e139f84f0fb942e0cbe557ddcad5b606cbbe4
B Ambrose <benjamin.ambrose2003@yahoo.com>
2023-02-14 16:27:08 +0000

    split html up into different files for clean code

---
+++ resources/views/weather-card.blade.php
@@ -0,0 +1,96 @@
+{{--Weather Card--}}
+<body class="mx-auto bg-blue-200 bg-gradient-to-tr from-blue-200 to-blue-400 min-h-screen">
+<div class="mt-8">
+    <div class="mx-auto bg-gray-900 text-white text-sm rounded-lg overflow-hidden" style="width: 550px">
+        <div class="current-weather flex items-center sm:justify-between px-4 py-6">
+            <div class="flex items-center">
+                <div>
+                    <div class="text-5xl font-semibold">4&#176;C</div>
+                    <div class="text-gray-400 font-semibold mt-2">Feels like 5&#176;C</div>
+                </div>
+                <div class="ml-5">
+                    <div class="font-semibold">Overcast</div>
+                    <div class="text-gray-400">Colchester, England</div>
+                </div>
+            </div>
+            <div>icon</div>
+        </div>
+        <ul class="future-weather dark:bg-gray-800 px-4 py-6 space-y-8">
+            <li class="items-center">
+                <div class="row">
+                    <div class="col-2 text-gray-400">TUE</div>
+                    <div class="col-1 flex-items-center">
+                        <div>icon</div>
+                    </div>
+                    <div class="col-7 flex-items-center">
+                        <div>Clear Sky</div>
+                    </div>
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+                    </div>
+                </div>
+                <br><br>
+                <div class="row">
+                    <div class="col-2 text-gray-400">TUE</div>
+                    <div class="col-1 flex-items-center">
+                        <div>icon</div>
+                    </div>
+                    <div class="col-7 flex-items-center">
+                        <div>Clear Sky</div>
+                    </div>
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+                    </div>
+                </div>
+                <br><br>
+                <div class="row">
+                    <div class="col-2 text-gray-400">TUE</div>
+                    <div class="col-1 flex-items-center">
+                        <div>icon</div>
+                    </div>
+                    <div class="col-7 flex-items-center">
+                        <div>Clear Sky</div>
+                    </div>
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+                    </div>
+                </div>
+                <br><br>
+                <div class="row">
+                    <div class="col-2 text-gray-400">TUE</div>
+                    <div class="col-1 flex-items-center">
+                        <div>icon</div>
+                    </div>
+                    <div class="col-7 flex-items-center">
+                        <div>Clear Sky</div>
+                    </div>
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+                    </div>
+                </div>
+                <br><br>
+                <div class="row">
+                    <div class="col-2 text-gray-400">TUE</div>
+                    <div class="col-1 flex-items-center">
+                        <div>icon</div>
+                    </div>
+                    <div class="col-7 flex-items-center">
+                        <div>Clear Sky</div>
+                    </div>
+                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+</div>
+
+
+</body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,123 +13,8 @@
     </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js">
     </script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        gridTemplateColumns: {
-                            'weather': '3fr 6fr 3fr',
-                        }
-                    }
-                }
-            }
-        }
-    </script>
-    <style type="text/tailwindcss">
-        @layer utilities {
-            .content-auto {
-                content-visibility: auto;
-            }
-        }
-    </style>
 </head>
-
-<body class="mx-auto bg-blue-200 bg-gradient-to-tr from-blue-200 to-blue-400 min-h-screen">
-<div class="mt-8">
-    <div class="mx-auto bg-gray-900 text-white text-sm rounded-lg overflow-hidden" style="width: 550px">
-        <div class="current-weather flex items-center sm:justify-between px-4 py-6">
-            <div class="flex items-center">
-                <div>
-                    <div class="text-5xl font-semibold">4&#176;C</div>
-                    <div class="text-gray-400 font-semibold mt-2">Feels like 5&#176;C</div>
-                </div>
-                <div class="ml-5">
-                    <div class="font-semibold">Overcast</div>
-                    <div class="text-gray-400">Colchester, England</div>
-                </div>
-            </div>
-            <div>icon</div>
-        </div>
-        <ul class="future-weather dark:bg-gray-800 px-4 py-6 space-y-8">
-            <li class="items-center">
-                <div class="row">
-                    <div class="col-2 text-gray-400">TUE</div>
-                    <div class="col-1 flex-items-center">
-                        <div>icon</div>
-                    </div>
-                    <div class="col-7 flex-items-center">
-                        <div>Clear Sky</div>
-                    </div>
-                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
-                        <div>4&#176;C</div>
-                        <div>4&#176;C</div>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-2 text-gray-400">TUE</div>
-                    <div class="col-1 flex-items-center">
-                        <div>icon</div>
-                    </div>
-                    <div class="col-7 flex-items-center">
-                        <div>Clear Sky</div>
-                    </div>
-                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
-                        <div>4&#176;C</div>
-                        <div>4&#176;C</div>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-2 text-gray-400">TUE</div>
-                    <div class="col-1 flex-items-center">
-                        <div>icon</div>
-                    </div>
-                    <div class="col-7 flex-items-center">
-                        <div>Clear Sky</div>
-                    </div>
-                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
-                        <div>4&#176;C</div>
-                        <div>4&#176;C</div>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-2 text-gray-400">TUE</div>
-                    <div class="col-1 flex-items-center">
-                        <div>icon</div>
-                    </div>
-                    <div class="col-7 flex-items-center">
-                        <div>Clear Sky</div>
-                    </div>
-                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
-                        <div>4&#176;C</div>
-                        <div>4&#176;C</div>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-2 text-gray-400">TUE</div>
-                    <div class="col-1 flex-items-center">
-                        <div>icon</div>
-                    </div>
-                    <div class="col-7 flex-items-center">
-                        <div>Clear Sky</div>
-                    </div>
-                    <div class="col-2 text-right text-xs" style="margin-top: -5px">
-                        <div>4&#176;C</div>
-                        <div>4&#176;C</div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-
-    </div>
-</div>
-
-
-</body>
+@include('weather-card')
 </html>
 
 
